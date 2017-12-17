f68815303fe0574e5e516a24ac608114aa707b23
wukai <wukai@wukai.local>
2017-12-17 10:45:47 +0800

    在1.7.0版本支持mqtt4

---
+++ ts_plug_mqtt3.1.1/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cp ./mqtt4.hrl ././../include/mqtt4.hrl
+cp ./ts_mqtt4.hrl ././../include/ts_mqtt4.hrl
+cp ./mqtt4_frame.erl ./../src/lib/mqtt4_frame.erl
+cp ./ts_config_mqtt4.erl ./../src/tsung_controller/ts_config_mqtt4.erl
+cp ./ts_mqtt4.erl ./../src/tsung/ts_mqtt4.erl
+
+cp ./tsung-1.0.dtd ./../tsung-1.0.dtd
+##进入上级目录
+
+cd ../
+
+./configure --prefix=/usr/local
+
+make uninstall
+
+make install
+
+
+
+
